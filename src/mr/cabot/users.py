d31c3cdb341d38a5f67b773a08fa40b3eaaff7b3
Matthew Wilkes <git@matthewwilkes.name>
2012-11-22 17:47:58 +0000

    Better handling of i18n names

--- src/mr/cabot/users.py
+++ src/mr/cabot/users.py
@@ -1,8 +1,22 @@
-from zope.component import getGlobalSiteManager
+import string
 
+from zope.component import getGlobalSiteManager
 from zope.interface import implements
 
 from mr.cabot.interfaces import IUserDatabase
+
+BAD = "AEIOUY"
+
+def name_cmp(one, two):
+    one = one.upper().encode("ascii","replace")
+    two = two.upper().encode("ascii","replace")
+    one = [l for l in one if l not in BAD and l not in string.uppercase]
+    two = [l for l in two if l not in BAD and l not in string.uppercase]
+    diffs = 0
+    for ch1, ch2 in zip(one, two):
+        if ch1 != ch2:
+            diffs += 1
+    return diffs < 3
 
 class User(object):
     
@@ -31,7 +45,7 @@ class Users(object):
         return [u for u in self.users if u.email == email][0]
 
     def get_user_by_name(self, name):
-        return [u for u in self.users if u.name == name][0]
+        return [u for u in self.users if name_cmp(u.name, name)][0]
 
 gsm = getGlobalSiteManager()
 gsm.registerUtility(Users())
