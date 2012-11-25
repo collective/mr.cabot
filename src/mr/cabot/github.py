1b7e71110669f3f6b78f14b80bc027d1f64ad288
Matthew Wilkes <git@matthewwilkes.name>
2012-11-25 06:17:48 +0000

    Populate user database with members of org

--- src/mr/cabot/github.py
+++ src/mr/cabot/github.py
@@ -24,17 +24,40 @@ class create(object):
         i = 0.0
         for account in accounts:
             i+=1
-            print "%2d%% complete" % ((i/len(accounts))*100.0)
+            if not (i % 10):
+                logger.info("github: geocoder: %2d%% complete" % ((i/len(accounts))*100.0))
             gh_api = urllib2.urlopen("https://api.github.com/users/%s?access_token=%s" % (account,token))
             user = json.loads(gh_api.read())
             try:
+                logger.debug("geocoder: Getting coordinates for %s" % (user['location']))
                 location = geocoder.geocode(user['location'])[0].coordinates
             except:
                 location = None
             users.add_user(User(user.get('name', user['login']), user.get('email', None), location))
     
+    def get_github_accounts(self):
+        url = "https://api.github.com/orgs/%s/members?access_token=%s" % (self.org, self.token)
+        while True:
+            logger.debug("github: getting %s" % url)
+            members_resp = urllib2.urlopen(url)
+            members = json.loads(members_resp.read())
+            all_members = set()
+            for member in members:
+                all_members.add(member['login'])
+            links = LINKS.findall(members_resp.headers.get('Link', ''))
+            links = {link[1]:link[0] for link in links}
+            if 'next' in links:
+                logger.debug("github: %s has too many users, requesting more" % (self.org))
+                url = links['next']
+            else:
+                logger.info("github: Got %s users for %s" % (len(members), self.org))
+                break
+        return self._get_github_accounts(all_members, self.token)
+    
     def get_data(self, token, checkout_directory):
         self.token = token
+        self.get_github_accounts()
+        
         if checkout_directory == "temp":
             checkout_directory = None
         
