709f5d7d0e3047bf7e5a9130a6630a3c8567dd21
Matthew Wilkes <git@matthewwilkes.name>
2013-01-05 02:00:41 +0000

    Download google static maps and cache them as local files

--- src/mr/cabot/googlestaticmap.py
+++ src/mr/cabot/googlestaticmap.py
@@ -1,6 +1,9 @@
+import datetime
 import urllib
+import os
 
 from mr.cabot.interfaces import IListing, IGeolocation
+import sebastian
 
 colors = {"commit": "green", "mailing-list": "blue", "answer": "yellow"}
 
@@ -12,7 +15,21 @@ def join(objs):
         if loc not in unique_locations:
             unique_locations.add(loc)
             markers += "&markers=%s" % static_map_marker(obj)
-    return "https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=terrain%s&sensor=false&center=tripoli" % (markers)
+    image_location = "http://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=terrain%s&sensor=false&center=tripoli" % (markers)
+    try:
+        image = urllib.urlopen(image_location).read()
+    except:
+        pass
+    else:
+        image_directory = os.path.join(sebastian.find_base(), "var", "staticmaps")
+        if not os.path.exists(image_directory):
+            os.mkdir(image_directory)
+        today = datetime.datetime.now().date()
+        base_path = os.path.join(image_directory, today.isoformat())
+        with open(base_path+".png", "wb") as todays_image:
+            todays_image.write(image)
+    return image_location
+    
 
 def static_map_marker(obj):
     loc = IGeolocation(obj).coords
