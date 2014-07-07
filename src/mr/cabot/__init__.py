ccedc34133ed1d00ba37d11dc1c2afde54bbc18f
Matthew Wilkes <git@matthewwilkes.name>
2014-07-07 02:23:42 +0100

    Initial pass at user management

--- src/mr/cabot/__init__.py
+++ src/mr/cabot/__init__.py
@@ -17,5 +17,8 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('new_user', '/+/edit')
+    config.add_route('view_user', '/{user}')
+    config.add_route('edit_user', '/{user}/edit')
     config.scan()
     return config.make_wsgi_app()
--- src/mr/cabot/views.py
+++ src/mr/cabot/views.py
@@ -1,10 +1,13 @@
+import pyramid.httpexceptions as exc
 from pyramid.response import Response
 from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
+import transaction
 
 from mr.cabot.models import (
     DBSession,
+    Contributor,
     Identity,
     Activity
     )
@@ -25,22 +28,52 @@ BADGES = [
      'answer': 100},
 ]
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    awards = {}
-    for badge in BADGES:
-        awards[badge['name']] = []
-    uri = request.GET.get('uri')
-    for identity in DBSession.query(Identity).filter(Identity.uri == uri):
-        for badge in BADGES:
-            if 'commit' in badge:
-                if DBSession.query(Activity).filter(Activity.identity_id==identity.id, Activity.type=='commit').count() > badge['commit']:
-                    awards[badge['name']].append(identity)
-            if 'question' in badge:
-                if DBSession.query(Activity).filter(Activity.identity_id==identity.id, Activity.type=='question').count() > badge['question']:
-                    awards[badge['name']].append(identity)
-            if 'answer' in badge:
-                if DBSession.query(Activity).filter(Activity.identity_id==identity.id, Activity.type=='answer').count() > badge['answer']:
-                    awards[badge['name']].append(identity)
-    return {'awards': awards, 'project': 'mr.cabot'}
+@view_config(route_name='view_user', renderer='templates/view.pt')
+def view_user(request):
+    contributor = request.matchdict['user']
+    contributor = DBSession.query(Contributor).get(contributor)
 
+    awards = []
+    commit = 0
+    question = 0
+    answer = 0
+    for identity in contributor.identities:
+        commit += DBSession.query(Activity).filter(Activity.identity_id==identity.id, Activity.type=='commit').count()
+        question += DBSession.query(Activity).filter(Activity.identity_id==identity.id, Activity.type=='question').count()
+        answer += DBSession.query(Activity).filter(Activity.identity_id==identity.id, Activity.type=='answer').count()
+    for badge in BADGES:
+        if 'commit' in badge:
+            if commit > badge['commit']:
+                awards.append(badge['name'])
+        if 'question' in badge:
+            if question > badge['question']:
+                awards.append(badge['name'])
+        if 'answer' in badge:
+            if answer > badge['answer']:
+                awards.append(badge['name'])
+    return {'awards': awards}
+
+@view_config(route_name='edit_user', renderer='templates/edit.pt')
+def edit_user(request):
+    contributor = request.matchdict['user']
+    contributor = DBSession.query(Contributor).get(contributor)
+    identities = DBSession.query(Identity).filter(Identity.contributor_id==None).order_by(Identity.uri)
+    return {'contributor': contributor, "available":identities}
+
+@view_config(route_name='edit_user', request_method="POST", renderer='templates/edit.pt')
+def edit_user_do(request):
+    contributor = request.matchdict['user']
+    contributor = DBSession.query(Contributor).get(contributor)
+
+    new_ident = DBSession.query(Identity).filter(Identity.uri==request.POST['to_add']).one()
+    new_ident.contributor_id = contributor.id
+    DBSession.add(new_ident)
+    return edit_user(request)
+
+@view_config(route_name='new_user')
+def new_user(request):
+    contrib = Contributor()
+    DBSession.add(contrib)
+    DBSession.flush()
+    return exc.HTTPFound(request.route_url("edit_user", user=contrib.id))
+    
